11e4326577cc0d6b9c1b68a89ab0ca660a0a4779
Sumit kachariya <114276572+Jalmit@users.noreply.github.com>
2024-05-25 12:49:54 +0530

    Update page not found

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 import Logo from "./images/logo1.png"
+import NotfoundImage from "./images/not_found.png"
 
-export { Logo }
+export { Logo, NotfoundImage }
--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './pageNotFound.css'
+import { NotfoundImage } from 'assets'
 
 function PageNotFound() {
-   return <div className="content">Page Not Found</div>
+   return <div className="content page_not_found_section">
+      <div className='page_not_found'>
+         <img src={NotfoundImage} style={{ maxWidth: "100%" }} />
+      </div>
+   </div>
 }
 
 export default PageNotFound
